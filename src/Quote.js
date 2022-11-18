e2548b9d91aa25c45c032a1dd6e0bd9a5745b5a3
Izaan Khalid <izaan.khalid@outlook.com>
2022-11-17 20:54:46 -0600

    FINAL COMIT - Project Completed!!

--- src/Quote.js
+++ src/Quote.js
@@ -1,63 +1,78 @@
 const Quotes = [
     {
         "text": "Genius is one percent inspiration and ninety-nine percent perspiration.",
-        "author": "Thomas Edison"
+        "author": "Thomas Edison",
+        "color": "#93597b"
     },
     {
         "text": "You can observe a lot just by watching.",
-        "author": "Yogi Berra"
+        "author": "Yogi Berra",
+        "color": "#cdaf48"
     },
     {
         "text": "A house divided against itself cannot stand.",
-        "author": "Abraham Lincoln"
+        "author": "Abraham Lincoln",
+        "color": "#a0c407"
     },
     {
         "text": "Difficulties increase the nearer we get to the goal.",
-        "author": "Johann Wolfgang von Goethe"
+        "author": "Johann Wolfgang von Goethe",
+        "color": "#c4ecb3"
     },
     {
         "text": "Fate is in your hands and no one elses",
-        "author": "Byron Pulsifer"
+        "author": "Byron Pulsifer",
+        "color": "#4bada0"
     },
     {
         "text": "Be the chief but never the lord.",
-        "author": "Lao Tzu"
+        "author": "Lao Tzu",
+        "color": "#23bb7f"
     },
     {
         "text": "Nothing happens unless first we dream.",
-        "author": "Carl Sandburg"
+        "author": "Carl Sandburg",
+        "color": "#6c6990"
     },
     {
         "text": "Well begun is half done.",
-        "author": "Aristotle"
+        "author": "Aristotle",
+        "color": "#e7b181"
     },
     {
         "text": "Life is a learning experience, only if you learn.",
-        "author": "Yogi Berra"
+        "author": "Yogi Berra",
+        "color": "#7f366b"
     },
     {
         "text": "Self-complacency is fatal to progress.",
-        "author": "Margaret Sangster"
+        "author": "Margaret Sangster",
+        "color": "#d786f3"
     },
     {
         "text": "Peace comes from within. Do not seek it without.",
-        "author": "Buddha"
+        "author": "Buddha",
+        "color": "#fef30a"
     },
     {
         "text": "What you give is what you get.",
-        "author": "Byron Pulsifer"
+        "author": "Byron Pulsifer",
+        "color": "#f6d9c6"
     },
     {
         "text": "We can only learn to love by loving.",
-        "author": "Iris Murdoch"
+        "author": "Iris Murdoch",
+        "color": "#052dd8"
     },
     {
         "text": "Life is change. Growth is optional. Choose wisely.",
-        "author": "Karen Clark"
+        "author": "Karen Clark",
+        "color": "#d0d0bc"
     },
     {
         "text": "You'll see it when you believe it.",
-        "author": "Wayne Dyer"
+        "author": "Wayne Dyer",
+        "color": "#a5133f"
     },
 ];
 
--- src/QuoteGenerator.jsx
+++ src/QuoteGenerator.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import Quotes from "./Quote"
+import Quotes from "./Quote";
 
 class QuoteGenerator extends React.Component {
   constructor(props){
@@ -17,12 +17,15 @@ class QuoteGenerator extends React.Component {
     });
   }
   render() {
+    document.body.style.backgroundColor = Quotes[this.state.index].color;
     return(
       <div id = "quote-box">
-        <h2 id = "text">{Quotes[this.state.index].text}</h2>
-        <p id = "author">- {Quotes[this.state.index].author}</p>
-        <button id = "new-quote" onClick={this.handleClick}>New Quote</button>
-        <a id = "tweet-quote" href='https://twitter.com/intent/tweet?text=hello%20world' target = "_blank" rel="noreferrer">test</a>
+        <h2 id = "text" style={{color: Quotes[this.state.index].color}}><i class="fas fa-quote-left"></i> {Quotes[this.state.index].text}</h2>
+        <p id = "author" style={{color: Quotes[this.state.index].color}}>- {Quotes[this.state.index].author}</p>
+        <div id = "buttons">
+          <button id = "new-quote" onClick={this.handleClick} style={{backgroundColor: Quotes[this.state.index].color}}>New Quote</button>
+          <a id = "tweet-quote" href='https://twitter.com/intent/tweet?text=hello%20world' target = "_blank" rel="noreferrer" style={{backgroundColor: Quotes[this.state.index].color}}><i class = "fab fa-twitter"></i></a>
+        </div>
       </div>
     );
   }
